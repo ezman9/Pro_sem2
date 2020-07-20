0af30d4f8be8bfccb5f5c3f63da2cd2f819a4d6e
Hagimoli <hagimoli0123@gmail.com>
2020-07-20 20:15:07 +0700

    Databasehelper

--- src/main/java/com/mycompany/project_sem2/helpers/DatabaseHelper.java
+++ src/main/java/com/mycompany/project_sem2/helpers/DatabaseHelper.java
@@ -5,10 +5,19 @@
  */
 package com.mycompany.project_sem2.helpers;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author Hi-XV
  */
 public class DatabaseHelper {
-    
+    public static Connection openConnection() throws Exception{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            String connectionUrl = "jdbc:sqlserver://localhost;database=DemoDB;";  
+            Connection con = DriverManager.getConnection(connectionUrl,"sa","0123");
+            
+            return con;
+    }
 }
